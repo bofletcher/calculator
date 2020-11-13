e875556ce4ce2ee480ec8041b9bcf27b7869b160
bofletcher <hfletche.hf@gmail.com>
2020-11-12 19:48:48 -0500

    added events

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
 const calculator = {
-  displayValue: 0,
+  displayValue: "",
   firstOperand: null,
   waitingForSecondOperand: false,
   operator: null,
 }
+
+const displayValue = document.getElementById("input-display");
+displayValue.value = calculator.displayValue
+
+const wrapper = document.getElementById("button-container");
+wrapper.addEventListener("click", buttonClick)
+
+
+function buttonClick(event) {
+  let input = event.target.value;
+  if(input == "=" && input != undefined) {
+    calculateTotal()
+  } else if (input == "+" ||input == "-"|| input == "*" || input == "/") {
+      handleOperator(input)
+  } else {
+    let inuputNum = parseInt(input);
+    console.log(input)
+    updateDisplay(input)
+  }
+}
+
+function updateDisplay(value) {
+  calculator.displayValue += value
+  displayValue.value = calculator.displayValue
+}
+
+function handleOperator(input) {
+  calculator.operator = input;
+  console.log(calculator.operator)
+}
+
+function calculateTotal() {
+ console.log("Total")
+}
+
